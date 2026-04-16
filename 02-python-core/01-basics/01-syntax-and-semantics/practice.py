68490788ac7a3bd839132db2ec107557661f58b8
Silver Bullet <aahishsingh2000@outlook.com>
2026-04-16 16:49:38 +0530

    feat: add practice.py

---
+++ 02-python-core/01-basics/01-syntax-and-semantics/practice.py
@@ -0,0 +1,21 @@
+# Case sensitivity demo
+name = "Aahish"
+Name = "AI"
+
+print("name:", name)
+print("Name:", Name)
+
+
+# Indentation demo
+age = 32
+
+if age > 30:
+    print("Age is greater than 30")
+
+
+# Dynamic typing
+var = 10
+print(type(var))
+
+var = "Aahish"
+print(type(var))
