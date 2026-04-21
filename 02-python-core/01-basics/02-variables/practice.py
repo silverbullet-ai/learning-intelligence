63907ad5e320259026682db87c798b64b108acb6
Silver Bullet <aahishsingh2000@outlook.com>
2026-04-21 16:25:18 +0530

    feat: add python variables with examples, practice, and mini calculator project

---
+++ 02-python-core/01-basics/02-variables/practice.py
@@ -0,0 +1,17 @@
+# Variable creation
+name = "Aahish"
+age = 21
+
+print("My name is", name)
+print("I am", age, "years old")
+
+# Type checking
+print(type(name))
+print(type(age))
+
+# Dynamic typing
+var = 10
+print(type(var))
+
+var = "Now string"
+print(type(var))
