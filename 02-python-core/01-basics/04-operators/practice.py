d8123cc4cc22bd6991b548e344353b1a6631fe25
Silver Bullet <aahishsingh2000@outlook.com>
2026-04-22 18:01:21 +0530

    feat: add python operators with arithmetic, comparison, logical examples and calculator

---
+++ 02-python-core/01-basics/04-operators/practice.py
@@ -0,0 +1,15 @@
+a = 7
+b = 3
+
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+
+# Comparison
+print(a > b)
+print(a == b)
+
+# Logical
+print(True and False)
+print(True or False)
+print(not True)
