112c69e5d004d1ad89a335470390a09b6541035b
Silver Bullet <aahishsingh2000@outlook.com>
2026-04-23 22:20:58 +0530

    feat(control-flow): add conditional statements with practice, leap year, and calculator

---
+++ 02-python-core/02-control-flow/01-conditional-statements/practice.py
@@ -0,0 +1,18 @@
+# Even or Odd checker
+num = int(input("Enter number: "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# Positive / Negative / Zero
+num = int(input("Enter number: "))
+
+if num > 0:
+    print("Positive")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative")
