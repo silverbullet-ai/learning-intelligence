c13598cf33098381685ca8af0eaac10c5425c240
Silver Bullet <aahishsingh2000@outlook.com>
2026-05-06 20:44:24 +0530

    feat(data-structures): add python lists with slicing, methods, and list comprehension

---
+++ 02-python-core/03-data-structures/01-lists/mini_problems.py
@@ -0,0 +1,13 @@
+# Reverse without reverse()
+nums = [1, 2, 3, 4, 5]
+print(nums[::-1])
+
+# Remove duplicates
+nums = [1, 2, 2, 3, 4, 4]
+unique = list(set(nums))
+print(unique)
+
+# Max and Min
+nums = [5, 2, 8, 1]
+print(max(nums))
+print(min(nums))
