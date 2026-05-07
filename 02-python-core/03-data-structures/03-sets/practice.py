ea924fc739141ba19cc40d8478384e6a4b5e2247
Silver Bullet <aahishsingh2000@outlook.com>
2026-05-07 21:46:32 +0530

    feat(data-structures): add sets with operations, duplicate removal, and practice examples

---
+++ 02-python-core/03-data-structures/03-sets/practice.py
@@ -0,0 +1,24 @@
+# Creating sets
+my_set = {1, 2, 3, 4}
+
+print(my_set)
+
+# Duplicate removal
+nums = {1, 2, 2, 3, 4, 4, 5}
+
+print(nums)
+
+# Adding elements
+my_set.add(5)
+print(my_set)
+
+# Removing elements
+my_set.remove(2)
+print(my_set)
+
+# Membership test
+print(3 in my_set)
+
+# Iterating
+for item in my_set:
+    print(item)
