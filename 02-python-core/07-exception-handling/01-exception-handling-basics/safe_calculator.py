1a6a8b7d19a51fe9cbf4381da94417ca7b8fe9df
Silver Bullet <aahishsingh2000@outlook.com>
2026-05-28 19:56:58 +0530

    feat(exception-handling): add python exception handling with try-except-finally examples

---
+++ 02-python-core/07-exception-handling/01-exception-handling-basics/safe_calculator.py
@@ -0,0 +1,15 @@
+try:
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print("Addition:", num1 + num2)
+    print("Division:", num1 / num2)
+
+except ZeroDivisionError:
+
+    print("Denominator cannot be zero")
+
+except ValueError:
+
+    print("Please enter valid numbers")
